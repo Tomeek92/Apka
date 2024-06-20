6aee7fb75b0516fbd2ba9333e72a88cc3b22aeab
Tomasz Fryckowski <tomaszfryckowski@gmail.com>
2024-06-20 14:08:15 +0200

    Dodaj pliki projektów.

---
+++ ApkaAnalizatorInfrastructure/Migrations/20240620102523_init2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApkaAnalizatorInfrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ZAplikacjiKrzysztofa",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    InsertName = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    DateTimeInsert = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    NumberProtocol = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    Description = table.Column<string>(type: "NVARCHAR2(2000)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ZAplikacjiKrzysztofa", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ZAplikacjiKrzysztofa");
+        }
+    }
+}
